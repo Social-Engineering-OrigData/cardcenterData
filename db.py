ee7400b2545660013dd0083564d699ee752bc843
GUI-nephelo <1736319094@qq.com>
2021-05-09 21:30:10 +0800

    as

--- db.py
+++ db.py
@@ -9,8 +9,8 @@ if __name__=="__main__":
     noL = []
     with open("./no.list","r") as f:
         noL = [i[:8] for i in f.readlines()]
-    workL = set([i[:8] for i in list(os.walk("./data"))[0][2]]) - set([i[0] for i in cur.execute("select id from basicData;").fetchall()])-set(noL)
-    print(workL)
+    workL = set([i[:8] for i in list(os.walk("./data"))[0][2]]) - set([i[0] for i in cur.execute("select id from HighSchool;").fetchall()])-set(noL)-set([i[0] for i in cur.execute("select id from JuniorSchool;").fetchall()])
+    print(len(workL))
     for i in workL:
         print(i)
         d = "./data/"+i+".json"
@@ -27,13 +27,14 @@ if __name__=="__main__":
                 picUrl = r["picUrl"]
                 schoolN = r["office"]["name"]
                 schoolC = r["office"]["code"]
-                
-                cur.execute("insert into basicData values ('%s','%s','%s','%s','%s','%s','%s','%s','%s')"%(id,name,puid,grade,gender,schoolN,schoolC,classNo,picUrl))
+                sL = r["schoolLevel"]
+                #print(sL)
+                cur.execute("insert into %s values ('%s','%s','%s','%s','%s','%s','%s','%s','%s')"%(("HighSchool" if sL=="3"or sL=="0" else ("JuniorSchool" if sL=="2" else ("SorPSchool" if sL=="1" else None))),id,name,puid,grade,gender,schoolN,schoolC,classNo,picUrl))
             else:
                 with open("./no.list","a") as f:
                     f.write(i+"\n")
                 
-    #conn.commit()
+    conn.commit()
     """
     /cmisfolder/photos/2012003/2021/01034018/09067530.JPG
     a=[(1 if i[:2]=="11" else 0) for i in list(os.walk("./data"))[0][2]]
